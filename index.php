8f7c4ddb7a0017de7cc6cda3f8739ce234713d15
geekahedron <d_j_@hotmail.com>
2016-10-26 15:25:57 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once("content.php");
+
+printPageHeader();
+
+printHeader();
+printMenu();
+printContent();
+printFooter();
+
+printErrors();
+printMessages();
+
+printPageFooter();
+
+display();
+?>
